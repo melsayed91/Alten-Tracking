e564c492979eb3ae6d58c7f1271e36bbba24f479
Mohamed Alaa <malaa@prototype.net>
2019-05-08 12:15:54 +0400

    change UI config

--- alten.frontend/src/environments/environment.prod.ts
+++ alten.frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  driverAPIUrl: 'http://35.226.207.117:3000',
-  vehicleAPIUrl: 'http://35.226.80.98:3001',
-  monitoringUrl: 'http://35.202.25.240:3002'
+  driverAPIUrl: 'http://35.225.203.28:3000',
+  vehicleAPIUrl: 'http://35.232.169.239:3001',
+  monitoringUrl: 'http://104.197.89.110:3002'
 };
